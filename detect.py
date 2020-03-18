96123b722007b2d749bf05b4c8b66bd4efbacb40
yl305237731 <yl305237731@gmail.com>
2020-03-18 19:24:51 +0800

    add

---
+++ detect.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import cv2
+import torch.backends.cudnn as cudnn
+import time
+from vision.yolo import Yolo_V3
+from tools.voc_data import labels
+from tools.utils import decode_netout, correct_yolo_boxes, do_nms, draw_boxes, preprocess_input
+
+
+parser = argparse.ArgumentParser(description='Test')
+parser.add_argument('-m', '--trained_model', default='./weights/Final.pth',
+                    type=str, help='Trained state_dict file path to open')
+parser.add_argument('--save_folder', default='./result', type=str, help='Dir to save img')
+parser.add_argument('--cpu', default=True, help='Use cpu inference')
+parser.add_argument('--confidence_threshold', default=0.5, type=float, help='confidence_threshold')
+parser.add_argument('--nms_threshold', default=0.3, type=float, help='nms_threshold')
+parser.add_argument('--anchors', default=[[22, 42], [47, 138], [67, 66], [85, 227], [140, 127], [146, 288], [231, 335],
+                                          [294, 183], [366, 355]], type=list, help="anchor size[w, h]")
+parser.add_argument('--net_w', default=416, type=int)
+parser.add_argument('--net_h', default=416, type=int)
+parser.add_argument('--input_path', default='./data/', type=str, help="image or images dir")
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    torch.set_grad_enabled(False)
+
+    net_w = args.net_w
+    net_h = args.net_h
+
+    save_folder = args.save_folder
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+
+    cpu = args.cpu
+    confidence_threshold = args.confidence_threshold
+    nms_thresh = args.nms_threshold
+    anchors = args.anchors
+    class_num = len(labels)
+    device = torch.device("cpu" if cpu else "cuda")
+
+    net = Yolo_V3(class_num=class_num, anchors=[3, 3, 3])
+    net.load_state_dict(torch.load(args.trained_model, map_location=torch.device(device)))
+    net.eval()
+
+    cudnn.benchmark = True
+    net = net.to(device)
+
+    input_path = args.input_path
+    image_paths = []
+
+    if os.path.isdir(input_path):
+        for inp_file in os.listdir(input_path):
+            image_paths += [input_path + inp_file]
+    else:
+        image_paths += [input_path]
+
+    image_paths = [inp_file for inp_file in image_paths if (inp_file[-4:] in ['.jpg', '.png', 'JPEG'])]
+
+    for img_path in image_paths:
+        begin = time.time()
+        print("Detect {}".format(img_path))
+        image = cv2.imread(img_path)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        img_h, img_w, _ = img.shape
+        img = preprocess_input(img, net_h, net_w).to(device)
+        net_out = net(img)
+        boxes = decode_netout(net_out, anchors=anchors, confidence_thresh=confidence_threshold, net_w=net_w, net_h=net_h)
+        correct_yolo_boxes(boxes, img_h, img_w, net_h, net_w)
+        do_nms(boxes, nms_thresh)
+        image = draw_boxes(image, boxes, labels)
+        cv2.imwrite(os.path.join(save_folder, img_path.split('/')[-1]), image)
+        end = time.time()
+        print("per image tiem: {}".format(end - begin))
+
+    print("Done!!!")
--- tools/utils.py
+++ tools/utils.py
@@ -1,8 +1,9 @@
-import torch
 import xml.etree.ElementTree as ET
 import os
-import cv2
 import numpy as np
+import cv2
+import torchvision.transforms as transforms
+import torch
 
 
 def anchor_iou(box, anchor):
@@ -176,3 +177,126 @@ def parse_wider_annotation(label_txt_path, img_root):
     if len(img['object']) > 0:
         all_insts += [img]
     return all_insts
+
+
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, c, classes=None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+
+        self.c = c
+        self.classes = classes
+        self.label = np.argmax(self.classes)
+        self.score = -1
+
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.label >= 0 if self.label else 0]
+
+        return self.score
+
+
+def decode_netout(netout, anchors, confidence_thresh, net_h=416, net_w=416):
+    boxes = []
+    anchors = anchors[::-1]
+    for level in range(len(netout)):
+        anchor = anchors[level * 3: level * 3 + 3][::-1]
+        netout_cur = torch.squeeze(netout[level], dim=0)
+        grid_h, grid_w = netout_cur.shape[:2]
+        nb_box = len(anchor)
+        netout_cur = netout_cur.view(nb_box, -1, grid_w * grid_h)
+        netout_mask = (netout_cur[:, 4, :].sigmoid_() >= confidence_thresh).float()
+        obj_grids = list(zip(*torch.where(netout_mask == 1)))
+
+        for ii in obj_grids:
+            anchor_index = ii[0]
+            grid_index = ii[1]
+            box = netout_cur[anchor_index, :, grid_index]
+            row = grid_index // grid_h
+            col = grid_index % grid_w
+            x, y, w, h = box[0].sigmoid(), box[1].sigmoid(), box[2], box[3]
+            anchor_w = anchor[anchor_index][0]
+            anchor_h = anchor[anchor_index][1]
+            x = (x + col) * (net_w / grid_w)
+            y = (y + row) * (net_h / grid_h)
+            w = anchor_w * np.exp(w)
+            h = anchor_h * np.exp(h)
+            with torch.no_grad():
+                classes = torch.sigmoid(box[5:])
+            box = BoundBox(x - w / 2, y - h / 2, x + w / 2, y + h / 2, box[4], classes)
+            boxes.append(box)
+    return boxes
+
+
+def correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w):
+
+    for i in range(len(boxes)):
+
+        boxes[i].xmin = int(boxes[i].xmin * image_w / net_w)
+        boxes[i].xmax = int(boxes[i].xmax * image_w / net_w)
+        boxes[i].ymin = int(boxes[i].ymin * image_h / net_h)
+        boxes[i].ymax = int(boxes[i].ymax * image_h / net_h)
+
+
+def iou(box_a, box_b):
+    """
+    :param box_a: [x1, y1, x2, y2]
+    :param box_b: [x1, y1, x2, y2]
+    :return: iou
+    """
+    area_boxa = (box_a.xmax - box_a.xmin) * (box_a.ymax - box_a.ymin)
+    area_boxb = (box_b.xmax - box_b.xmin) * (box_b.ymax - box_b.ymin)
+
+    def intersection(box1, box2):
+        x_lt = max(box1.xmin, box2.xmin)
+        y_lt = max(box1.ymin, box2.ymin)
+        x_br = min(box1.xmax, box2.xmax)
+        y_br = min(box1.ymax, box2.ymax)
+        inter_w = max(x_br - x_lt, 0)
+        inter_h = max(y_br - y_lt, 0)
+        return float(inter_w * inter_h)
+    area_inter = intersection(box_a, box_b)
+    return area_inter / (area_boxa + area_boxb - area_inter)
+
+
+def do_nms(boxes, nms_thresh=0.4):
+    if len(boxes) > 0:
+        nb_class = len(boxes[0].classes)
+    else:
+        return
+
+    for c in range(nb_class):
+        sorted_indices = np.argsort([-box.classes[c] for box in boxes])
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+            if boxes[index_i].classes[c] == 0: continue
+            for j in range(i+1, len(sorted_indices)):
+                index_j = sorted_indices[j]
+                if iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
+                    boxes[index_j].classes[c] = 0
+                    boxes[index_j].label = -1
+
+
+def preprocess_input(image, net_w, net_h):
+    resized = cv2.resize(image, (net_w, net_h))
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+    return torch.unsqueeze(transform(resized), dim=0)
+
+
+def draw_boxes(image, boxes, labels):
+    for box in boxes:
+        label = box.label
+        if label >= 0:
+            label_str = str(labels[label] + ',' + str(box.c.numpy()))
+            cv2.rectangle(img=image, pt1=(box.xmin, box.ymin), pt2=(box.xmax, box.ymax), color=(255, 0, 255), thickness=2)
+            cv2.putText(img=image,
+                        text=label_str,
+                        org=(box.xmin + 13, box.ymin - 13),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1e-3 * image.shape[0],
+                        color=(255, 0, 0),
+                        thickness=2)
+    return image
